c260136b3f72daf86c043c3340f1b072906c1d97
Tomo Masakura <tomo.masakura@gmail.com>
2015-03-10 22:33:00 +0900

    ユーザー名に合わせてリポジトリリストが変わるようにしてみた

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,29 @@
  */
 angular.module('angularMvcSampleApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.userInput = '';
     $scope.repositories = [];
 
     $scope.fetch = function () {
-      $scope.repositories = [
-        {name: 'generator-graybullet-cordova', stars: 6},
-        {name: 'grunt-cordova-ng', stars: 2},
-        {name: 'testmator', stars: 0}
-      ];
+      switch ($scope.userInput) {
+      case 'GrayBullet':
+        $scope.repositories = [
+          {name: 'generator-graybullet-cordova', stars: 6},
+          {name: 'grunt-cordova-ng', stars: 2},
+          {name: 'testmator', stars: 0}
+        ];
+        break;
+
+      case 'masakura':
+        $scope.repositories = [
+          {name: 'sample0', stars: 1},
+          {name: 'sample1'}
+        ];
+        break;
+
+      default:
+        $scope.repositories = [];
+        break;
+      }
     };
   });
