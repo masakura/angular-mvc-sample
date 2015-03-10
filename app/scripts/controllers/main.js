132497541917e61855b600a805f97dcc1be13695
Tomo Masakura <tomo.masakura@gmail.com>
2015-03-10 22:35:20 +0900

    モデル部分を抽出してみた

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,37 @@
  * Controller of the angularMvcSampleApp
  */
 angular.module('angularMvcSampleApp')
+  .factory('repositoryLibrary', function () {
+    var library = {};
+
+    library.userInput = '';
+    library.repositories = [];
+
+    library.fetch = function () {
+      switch (library.userInput) {
+      case 'GrayBullet':
+        library.repositories = [
+          {name: 'generator-graybullet-cordova', stars: 6},
+          {name: 'grunt-cordova-ng', stars: 2},
+          {name: 'testmator', stars: 0}
+        ];
+        break;
+
+      case 'masakura':
+        library.repositories = [
+          {name: 'sample0', stars: 1},
+          {name: 'sample1'}
+        ];
+        break;
+
+      default:
+        library.repositories = [];
+        break;
+      }
+    };
+
+    return library;
+  })
   .controller('MainCtrl', function ($scope) {
     $scope.userInput = '';
     $scope.repositories = [];
