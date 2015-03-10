3f24e40fba0c487214d1f0141a69c4fde5ffbbbc
Tomo Masakura <tomo.masakura@gmail.com>
2015-03-10 22:46:07 +0900

    不要になった処理を除去

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -34,28 +34,6 @@ angular.module('angularMvcSampleApp')
       repositories: [],
       fetch: function () {
         this.repositories = getRepositories(this.userInput);
-        /*
-        switch (this.userInput) {
-        case 'GrayBullet':
-          this.repositories = [
-            {name: 'generator-graybullet-cordova', stars: 6},
-            {name: 'grunt-cordova-ng', stars: 2},
-            {name: 'testmator', stars: 0}
-          ];
-          break;
-
-        case 'masakura':
-          this.repositories = [
-            {name: 'sample0', stars: 1},
-            {name: 'sample1'}
-          ];
-          break;
-
-        default:
-          this.repositories = [];
-          break;
-        }
-         */
       }
     };
   })
