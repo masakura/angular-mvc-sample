4feede8519e91573d32a5b64a9dede95e6167b1c
Tomo Masakura <tomo.masakura@gmail.com>
2015-03-10 22:43:31 +0900

    ダミーリストを返す型を作成してみた

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,35 @@
  * Controller of the angularMvcSampleApp
  */
 angular.module('angularMvcSampleApp')
+  .factory('DummyRepositoryService', function () {
+    var getRepositories = function (user) {
+      switch (user) {
+      case 'GrayBullet':
+        return [
+          {name: 'generator-graybullet-cordova', stars: 6},
+          {name: 'grunt-cordova-ng', stars: 2},
+          {name: 'testmator', stars: 0}
+        ];
+
+      case 'masakura':
+        return [
+          {name: 'sample0', stars: 1},
+          {name: 'sample1'}
+        ];
+
+      default:
+        return [];
+      }
+    };
+
+    var DummyRepositoryService = function () {};
+
+    DummyRepositoryService.prototype.fetch = function (user) {
+      return getRepositories(user);
+    };
+
+    return DummyRepositoryService;
+  })
   .factory('repositoryLibrary', function () {
     var getRepositories = function (user) {
       switch (user) {
